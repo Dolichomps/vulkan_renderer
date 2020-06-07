015b24e3bc1938ed960a28f4b3564662953cba36
Jordan Tully <jtullyoops@gmail.com>
2020-06-07 19:34:56 -0300

    add validation layers

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,80 @@
 use ash::{
     version::{EntryV1_0, InstanceV1_0},
-    vk, Device, Entry, Instance,
+    vk,
 };
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let entry = Entry::new()?;
-    let instance: Instance = unsafe { entry.create_instance(&Default::default(), None)? };
-    unsafe { instance.destroy_instance(None) };
+    let entry = ash::Entry::new()?;
+
+    // setup varaibles for ApplicationInfo
+    let engine_name = std::ffi::CString::new("GameEngine").unwrap();
+    let app_name = std::ffi::CString::new("Rusty VK").unwrap();
+    let app_info = vk::ApplicationInfo::builder()
+        .application_name(&app_name)
+        .application_version(vk::make_version(0, 0, 1))
+        .engine_name(&engine_name)
+        .engine_version(vk::make_version(0, 1, 0))
+        .api_version(vk::make_version(1, 0, 106));
+
+    // load validation layers and enable DebugUtils extension
+    let layer_names: Vec<std::ffi::CString> =
+        vec![std::ffi::CString::new("VK_LAYER_KHRONOS_validation").unwrap()];
+    let layer_name_pointers: Vec<*const i8> = layer_names
+        .iter()
+        .map(|layer_name| layer_name.as_ptr())
+        .collect();
+    let extension_name_pointers: Vec<*const i8> =
+        vec![ash::extensions::ext::DebugUtils::name().as_ptr()];
+
+    // setup debug create info
+    let mut debug_create_info = vk::DebugUtilsMessengerCreateInfoEXT::builder()
+        .message_severity(
+            vk::DebugUtilsMessageSeverityFlagsEXT::WARNING
+                | vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE
+                | vk::DebugUtilsMessageSeverityFlagsEXT::INFO
+                | vk::DebugUtilsMessageSeverityFlagsEXT::ERROR,
+        )
+        .message_type(
+            vk::DebugUtilsMessageTypeFlagsEXT::GENERAL
+                | vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE
+                | vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION,
+        )
+        .pfn_user_callback(Some(vulkan_debug_utils_callback));
+
+    // setup instance creation info
+    let instance_create_info = vk::InstanceCreateInfo::builder()
+        .push_next(&mut debug_create_info)
+        .application_info(&app_info)
+        .enabled_layer_names(&layer_name_pointers)
+        .enabled_extension_names(&extension_name_pointers);
+
+    // create vulkan instance
+    let instance = unsafe { entry.create_instance(&instance_create_info, None)? };
+
+    // create debug messenger instance
+    let debug_utils = ash::extensions::ext::DebugUtils::new(&entry, &instance);
+    let utils_messenger =
+        unsafe { debug_utils.create_debug_utils_messenger(&debug_create_info, None)? };
+
+    // instance cleanup
+    unsafe {
+        debug_utils.destroy_debug_utils_messenger(utils_messenger, None);
+        instance.destroy_instance(None)
+    };
+
     Ok(())
+}
+
+// external function call to setup validation layer callbacks
+unsafe extern "system" fn vulkan_debug_utils_callback(
+    message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
+    message_type: vk::DebugUtilsMessageTypeFlagsEXT,
+    p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT,
+    _p_user_data: *mut std::ffi::c_void,
+) -> vk::Bool32 {
+    let message = std::ffi::CStr::from_ptr((*p_callback_data).p_message);
+    let severity = format!("{:?}", message_severity).to_lowercase();
+    let ty = format!("{:?}", message_type).to_lowercase();
+    println!("[Debug][{}][{}] {:?}", severity, ty, message);
+    vk::FALSE
 }
